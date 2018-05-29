cd5341fba319001f027c03c904f1a436d98c4369
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-05-29 16:35:00 -0300

    Fix bugs on html of user

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'users', component: UserComponent },
+  { path: '', component: AppComponent }
 ];
 
 @NgModule({
