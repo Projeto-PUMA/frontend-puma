8ce795b4a2ebfa527155fad150a67db55827e8b4
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-07-26 22:09:26 -0300

    Added validation for required fields.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,6 +6,7 @@ import {Http, Response} from "@angular/http";
 import {NgForm} from '@angular/forms';
 import { UserService } from '../user/user.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {FormControl, Validators} from '@angular/forms';
 
 @Injectable()
 @Component({
@@ -25,7 +26,61 @@ export class RegisterComponent implements OnInit {
   hideConfirm = true;
   constructor(private userService: UserService) {
   }
+  name = new FormControl('', [Validators.required]);
+  errorMessageName: string;
+  cpf = new FormControl('', [Validators.required]);
+  phone1 = new FormControl('', [Validators.required]);
+  phone2 = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  degree = new FormControl('', [Validators.required]);
+  errorMessageDegree: string;
+  profession = new FormControl('', [Validators.required]);
+  errorMessageProfession: string;
 
+  getErrorMessageName() {
+    this.errorMessageName = 'Campo não pode ser vazio.';
+    if(this.name.hasError('required')){
+      return this.errorMessageName;
+    }
+  }
+  
+  getErrorMessageCpf() {
+    return this.cpf.hasError('required') ? 'Campo não pode ser vazio.' :
+        this.cpf.hasError('cpf') ? 'CPF inválido.' :
+            '';
+  }
+
+  getErrorMessagePhone1() {
+    return this.phone1.hasError('required') ? 'Campo não pode ser vazio.' :
+        this.phone1.hasError('phone1') ? 'Telefone inválido.' :
+            '';
+  }
+
+  getErrorMessagePhone2() {
+    return this.phone2.hasError('required') ? 'Campo não pode ser vazio.' :
+        this.phone2.hasError('phone2') ? 'Telefone inválido.' :
+            '';
+  }
+
+  getErrorMessageEmail() {
+    return this.email.hasError('required') ? 'Campo não pode ser vazio.' :
+        this.email.hasError('email') ? 'Email deve estar no formato usuario@email.com.' :
+            '';
+  }
+
+  getErrorMessageDegree() {
+    this.errorMessageDegree = 'Selecione uma opção.';
+    if(this.degree.hasError('required')){
+      return this.errorMessageDegree;
+    }
+  }
+
+  getErrorMessageProfession() {
+    this.errorMessageProfession = 'Campo não pode ser vazio.';
+    if(this.profession.hasError('required')){
+      return this.errorMessageProfession;
+    }
+  }
   ngOnInit() {
   }
 
