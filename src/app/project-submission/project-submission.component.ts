76a3fe1fbde180d07fc7d602b322f4c0117bf587
thiagoiferreira <thiagoiferreira@outlook.com>
2018-08-17 12:53:34 -0300

    Adding error messages for project submmit page

--- src/app/project-submission/project-submission.component.ts
+++ src/app/project-submission/project-submission.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import {RequestOptions, Headers, Http } from '@angular/http';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
@@ -19,7 +20,26 @@ export class ProjectSubmissionComponent implements OnInit {
   pspNumbers: Psp[] = pspOptions;
   pspSubOption: Array<Object> = pspSubOptions;
   iterationPsp = [0, 1, 2, 3, 4, 5, 6];
+  
+  title = new FormControl('', [Validators.required]); //
+  resume = new FormControl('', [Validators.required]); //
+  problem = new FormControl('', [Validators.required]); //
+  personType = new FormControl('', [Validators.required]); //
+  cnpj = new FormControl('', [Validators.required]); //
+  applicationArea = new FormControl('', [Validators.required]); //
+  subArea = new FormControl('', [Validators.required]); //
+  socialReason = new FormControl('', [Validators.required]); //
+  fantasyName = new FormControl('', [Validators.required]); //
 
+  errorMessageTitle: string;
+  errorMessageResume: string;
+  errorMessageProblem: string;
+  errorMessagePersonType: string;
+  errorMessageCNPJ: string;
+  errorMessageApplicationArea: string;
+  errorMessageSubArea: string;
+  errorMessageSocialReason: string;
+  errorMessageFantasyName: string;
 
   constructor(
     public nav: NavbarService,
@@ -53,5 +73,60 @@ export class ProjectSubmissionComponent implements OnInit {
         //     )
     }
 }
+getErrorMessageTitle() {
+  this.errorMessageTitle = 'Campo Obrigatório';
+  if(this.title.hasError('required')){
+    return this.errorMessageTitle;
+  }
+}
+getErrorMessageResume() {
+  this.errorMessageResume = 'Campo Obrigatório';
+  if(this.resume.hasError('required')){
+    return this.errorMessageResume;
+  }
+}
+getErrorMessageProblem() {
+  this.errorMessageProblem = 'Campo Obrigatório';
+  if(this.problem.hasError('required')){
+    return this.errorMessageProblem;
+  }
+}
+getErrorMessagePersonType() {
+  this.errorMessagePersonType = 'Campo Obrigatório';
+  if(this.personType.hasError('required')){
+    return this.errorMessagePersonType;
+  }
+}
+getErrorMessageFantasyName() {
+  this.errorMessageFantasyName = 'Campo Obrigatório';
+  if(this.fantasyName.hasError('required')){
+    return this.errorMessageFantasyName;
+  }
+}
+getErrorMessageSocialReason() {
+  this.errorMessageSocialReason = 'Campo Obrigatório';
+  if(this.socialReason.hasError('required')){
+    return this.errorMessageSocialReason;
+  }
+}
+getErrorMessageCNPJ() {
+  this.errorMessageCNPJ = 'Campo Obrigatório';
+  if(this.cnpj.hasError('required')){
+    return this.errorMessageCNPJ;
+  }
+}
+getErrorMessagePsp() {
+  this.errorMessageApplicationArea = 'Campo Obrigatório';
+  if(this.applicationArea.hasError('required')){
+    return this.errorMessageApplicationArea;
+  }
+}
+getErrorMessagePspType() {
+  this.errorMessageSubArea = 'Campo Obrigatório';
+  if(this.subArea.hasError('required')){
+    return this.errorMessageSubArea;
+  }
+}
+
 
 }
