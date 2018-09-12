6afc72c7f62e235cbede7a8fe1521ef17f40f006
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-09-11 22:34:38 -0300

    Arrumando CORS

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express= require('express'),
 	 path = require('path');
 
 const app =express();
-
+const cors = require('cors');
 
 app.use(express.static('./dist/frontend-puma'));
-
+app.use(cors());
 app.get('/*', (req,res)=>{
     
 res.sendFile(path.join(__dirname,'/dist/frontend-puma/index.html'));
 
 });
-
-const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
-const express = require('express');
-const app = express();
-app.use(cors());
 
 app.listen(process.env.PORT || 8080, ()=>{
 console.log('Server started');
