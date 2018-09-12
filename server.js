a86cc561a6f59aae3136f73051bfe0c6fa84d526
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-09-11 22:27:10 -0300

    Adicionando CORS

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ res.sendFile(path.join(__dirname,'/dist/frontend-puma/index.html'));
 
 });
 
+const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
+const express = require('express');
+const app = express();
+app.use(cors());
+
 app.listen(process.env.PORT || 8080, ()=>{
 console.log('Server started');
 })
